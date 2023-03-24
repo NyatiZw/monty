c907c84125c773b25611a2945131f364e584f365
NyatiZw <appsmaid19@gmail.com>
2023-03-24 13:44:01 -0700

    fifo lifo monty

--- opcodes.c
+++ opcodes.c
@@ -49,3 +49,45 @@ void _push(stack_t **head, unsigned int number)
 		addqueue(head, n);
 	}
 }
+
+/**
+ * _pall - prints the stack
+ * @head: stack head
+ * @number: integer
+ * Return: void
+ */
+void _pall(stack_t **head, unsigned int number)
+{
+	stack_t *h;
+	(void)number;
+
+	h = *head;
+	if (h == NULL)
+	{
+		return;
+	}
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
+
+/**
+ * _pint - prints the top
+ * @head: stack head
+ * @counter: line number
+ * Return: void
+ */
+void _pint(stack_t **head, unsigned int number)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: Can't pint, stack is empty\n", number);
+		fclose(global_variable.fd);
+		free(global_variable.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
